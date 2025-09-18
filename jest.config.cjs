d20a9f865c99d437de24c93dce9ffcb39c8a5d08
Igor Ganapolsky <iganapolsky@gmail.com>
2025-09-18 10:51:12 -0400

    fix: update GitGuardian config to ignore dependency files
    - Add babel.config.*, tsconfig.json, package.json to ignore patterns
    - Prevents false positives on dependabot PRs
    - Should unblock PRs #204, #203 that are failing GitGuardian checks

---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  testEnvironment: 'node',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/*.spec.{ts,tsx}',
+    '!src/types/**/*',
+    '!src/constants/**/*',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+};
