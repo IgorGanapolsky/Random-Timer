2616ac193b9882cca5ee14fcc3060e5bc3366d2c
Igor Ganapolsky <iganapolsky@gmail.com>
2026-04-06 13:16:42 -0400

    chore(release): bump version to 1.3.17

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.iganapolsky.randomtimer"
         minSdk = 26
         targetSdk = ciTargetSdk ?: 35
-        versionCode = ciVersionCode ?: 1774900000
-        versionName = "1.3.16"
+        versionCode = ciVersionCode ?: 1774900001
+        versionName = "1.3.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
